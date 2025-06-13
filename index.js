4bdabeb3169c72a33525ca1f2266ff0a13182f01
nairx <nairxprv@gmail.com>
2025-06-13 10:42:18 +0530

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+
 import cors from "cors";
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.listen(8080, () => {
-  console.log("Server started");
+
+mongoose.connect("mongodb://localhost:27017/mudb").then(() => {
+  app.listen(8080, () => {
+    console.log("Server started");
+  });
 });
+
+const userSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  pass: { type: String },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -21,4 +36,16 @@ app.post("/products", async (req, res) => {
 
 app.get("/products", async (req, res) => {
   res.json(products);
+});
+
+app.post("/register", async (req, res) => {
+  const body = req.body;
+  const user = await userModel.create(body);
+  res.json(user);
+});
+
+app.post("/login", async (req, res) => {
+  const body = req.body;
+  const found = await userModel.findOne({ email: body.email, pass: body.pass });
+  res.json(found);
 });
